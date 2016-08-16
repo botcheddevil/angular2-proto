ed6e98b2ae7bf14b8d2b7a9e9a5ca1da441f8f9e
botcheddevil <anshukk@gmail.com>
2016-08-16 08:42:42 +0530

    Initiated framework for layouting

---
+++ app/templates/layouts/three-column.layout.component.ts
@@ -0,0 +1,11 @@
+import {
+    Component
+} from "@angular/core";
+
+@Component({
+    selector: 'niit-layout-three-column',
+    templateUrl: 'app/templates/layouts/three-column.layout.component.html'
+})
+export class ThreeColumnLayoutComponent {
+    constructor() {}
+}
