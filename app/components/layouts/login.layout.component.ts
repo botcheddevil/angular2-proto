244661306f5b660362dde6b689e1bcdf1a93276e
botcheddevil <anshukk@gmail.com>
2016-08-30 14:24:18 +0530

    login layout component

---
+++ app/components/layouts/login.layout.component.ts
@@ -0,0 +1,13 @@
+import {
+    Component
+} from "@angular/core";
+
+@Component({
+    inputs: ['usernamePlaceholder', 'passwordPlaceholder'],
+    selector: 'niit-layout-login',
+    templateUrl: 'app/components/layouts/login.layout.component.html',
+    styleUrls: ['app/components/layouts/login.layout.component.css']
+})
+export class LoginLayoutComponent {
+    constructor() {}
+}
