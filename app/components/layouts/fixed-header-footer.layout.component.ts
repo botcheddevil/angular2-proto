7783f5bf9d7e802a3df9936095969cf1940ad061
botcheddevil <anshukk@gmail.com>
2016-08-30 14:23:51 +0530

    added fixed header footer layout

---
+++ app/components/layouts/fixed-header-footer.layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'niit-layout-fixed-header-footer',
+    templateUrl: 'app/components/layouts/fixed-header-footer.layout.component.html',
+    styleUrls: ['app/components/layouts/fixed-header-footer.layout.component.css']
+})
+export class FixedHeaderFooterLayoutComponent {
+}
