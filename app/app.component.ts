e347cb62dce91ed43c9ff317bf51ab624ced8289
botcheddevil <anshukk@gmail.com>
2016-08-30 14:26:34 +0530

    playing with the framework

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
-import { LoginLayoutComponent } from './templates/layouts/login.layout.component';
+import { LoginComponent } from './components/general/login.component';
+import { ThreeColumnComponent } from './components/general/three-column.component';
+import { FixedHeaderFooterComponent } from './components/general/fixed-header-footer.component';
 
 @Component({
     selector: 'my-app',
-    directives: [LoginLayoutComponent],
-    templateUrl: ['app/app.component.html']
+    directives: [FixedHeaderFooterComponent],
+    templateUrl: 'app/app.component.html'
 })
 export class AppComponent {
 }
