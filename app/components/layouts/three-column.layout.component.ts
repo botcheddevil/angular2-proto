b88a357890ad18ef4ad3f54d00cf1e69415dee7e
botcheddevil <anshukk@gmail.com>
2016-08-30 14:24:54 +0530

    three column layout

---
+++ app/components/layouts/three-column.layout.component.ts
@@ -0,0 +1,12 @@
+import {
+    Component
+} from "@angular/core";
+
+@Component({
+    selector: 'niit-layout-three-column',
+    templateUrl: 'app/components/layouts/three-column.layout.component.html',
+    styleUrls: ['app/components/layouts/three-column.layout.component.css']
+})
+export class ThreeColumnLayoutComponent {
+    constructor() {}
+}
